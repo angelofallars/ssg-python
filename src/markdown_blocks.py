661b41fb68eee2406bdaada72acb66800ca6fa41
Angelo Fallaria <ba.fallaria@gmail.com>
2025-08-28 05:46:21 +0800

    feat: add `block_to_block_type` function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,6 @@
+from typing import Literal
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     lines = markdown.split("\n\n")
     lines = map(lambda line: line.strip(), lines)
@@ -5,3 +8,59 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     lines = list(lines)
 
     return lines
+
+
+type BlockType = Literal[
+    "Paragraph", "Heading", "Code", "Quote", "UnorderedList", "OrderedList"
+]
+
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+
+    # Heading
+    if len(lines) == 1:
+        first_line = lines[0]
+        first_words = first_line.split(" ")
+
+        is_header = True
+        char_count = 0
+        for char in first_words[0]:
+            char_count += 1
+            if char != "#" or char_count > 6:
+                is_header = False
+                break
+
+        if char_count == 0:
+            is_header = False
+
+        if is_header:
+            return "Heading"
+
+    # Code
+    if block.startswith("```") and block.endswith("```"):
+        return "Code"
+
+    # Quote
+    is_quote = all(map(lambda line: line.startswith("> "), lines))
+    if is_quote:
+        return "Quote"
+
+    # Unordered list
+    is_unordered_list = all(map(lambda line: line.startswith("- "), lines))
+    if is_unordered_list:
+        return "UnorderedList"
+
+    # Ordered list
+    is_ordered_list = True
+    for i, line in enumerate(lines):
+        number = i + 1
+        if not line.startswith(f"{number}. "):
+            is_ordered_list = False
+            break
+
+    if is_ordered_list:
+        return "OrderedList"
+
+    # Paragraph
+    return "Paragraph"
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestTextNode(unittest.TestCase):
@@ -41,7 +41,7 @@ If only you were here
 """,
                 [
                     "A picture of you reminds me\nHow the years have gone so lonely\nAnd why do you have to leave me\nWithout saying that you love me?",
-                    "I'm saying \"I love you\" again\nAre you listening?\nOpen your eyes once again\nLook at me crying",
+                    'I\'m saying "I love you" again\nAre you listening?\nOpen your eyes once again\nLook at me crying',
                     "If only you could hear me shout your name\nIf only you could feel my love again\nThe stars in the sky will never be the same\nIf only you were here",
                 ],
             ),
@@ -71,6 +71,131 @@ Run HTML, the programming language code outside of the browser.
         for input, expected in test_cases:
             actual = markdown_to_blocks(input)
             self.assertEqual(actual, expected)
+
+    def test_block_to_block_type(self):
+        test_cases: list[tuple[str, BlockType]] = [
+            (
+                "Hello world",
+                "Paragraph",
+            ),
+            (
+                "Line 1\nLine 2\nLine 3\nLine 4",
+                "Paragraph",
+            ),
+            (
+                "# It's me!",
+                "Heading",
+            ),
+            (
+                "## Two number signs",
+                "Heading",
+            ),
+            (
+                "### Three number signs",
+                "Heading",
+            ),
+            (
+                "#### Four number signs",
+                "Heading",
+            ),
+            (
+                "##### Five number signs",
+                "Heading",
+            ),
+            (
+                "###### Ninety-nine red balloons",
+                "Heading",
+            ),
+            (
+                "####### Too much number signs",
+                "Paragraph",
+            ),
+            (
+                "######## Too much number signs",
+                "Paragraph",
+            ),
+            (
+                "#not a header",
+                "Paragraph",
+            ),
+            (
+                """```
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello world!")
+}
+```""",
+                "Code",
+            ),
+            (
+                """``
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello world!")
+}
+``""",
+                "Paragraph",
+            ),
+            (
+                "> Tell me that you're alright",
+                "Quote",
+            ),
+            (
+                "> Tell me that you're alright\n> Everything is alright",
+                "Quote",
+            ),
+            (
+                ">Tell me that you're alright\n>Everything is alright",
+                "Paragraph",
+            ),
+            (
+                "- Cat\n- Dog\n- Horse\n- Pig",
+                "UnorderedList",
+            ),
+            (
+                "-Cat\n-Dog\n-Horse\n-Pig",
+                "Paragraph",
+            ),
+            (
+                "1. Apple\n2. Orange\n3. Banana\n4. Lemon\n5. Strawberry",
+                "OrderedList",
+            ),
+            (
+                "1. Apple\n2. Orange\n4. Banana\n3. Lemon\n5. Strawberry",
+                "Paragraph",
+            ),
+            (
+                "2. Apple\n2. Orange\n4. Banana\n4. Lemon\n5. Strawberry",
+                "Paragraph",
+            ),
+            (
+                "1.Apple\n2.Orange\n3.Banana\n4.Lemon\n5.Strawberry",
+                "Paragraph",
+            ),
+            # Edge cases
+            (
+                "",
+                "Paragraph",
+            ),
+            (
+                "   ",
+                "Paragraph",
+            ),
+        ]
+
+        for input, expected in test_cases:
+            actual = block_to_block_type(input)
+            try:
+                self.assertEqual(actual, expected)
+            except AssertionError as ae:
+                ae.add_note(f"Input:\n{input}")
+                raise ae
 
 
 if __name__ == "__main__":
